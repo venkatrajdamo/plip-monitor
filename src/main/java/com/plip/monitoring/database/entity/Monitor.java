53a37c079230e7bc044e13610a670cd66a5b98a6
Venkatraj <venkatrajdamo@gmail.com>
2025-05-07 22:36:44 -0700

    Docker Compose and Unit, Integration Test

---
+++ src/main/java/com/plip/monitoring/database/entity/Monitor.java
@@ -0,0 +1,25 @@
+package com.plip.monitoring.database.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Monitor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String url;
+    private String method;
+    private int intervalSeconds;
+    private boolean active;
+}
