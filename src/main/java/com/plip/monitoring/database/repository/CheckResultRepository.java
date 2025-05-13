9bcffa7620d8c74629d8a8272d4e2033491e0847
Venkatraj <venkatrajdamo@gmail.com>
2025-05-12 21:43:05 -0700

    New API - Get Health

--- src/main/java/com/plip/monitoring/database/repository/CheckResultRepository.java
+++ src/main/java/com/plip/monitoring/database/repository/CheckResultRepository.java
@@ -1,6 +1,8 @@
 package com.plip.monitoring.database.repository;
 
 import com.plip.monitoring.database.entity.CheckResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,8 @@ import java.util.List;
 
 @Repository
 public interface CheckResultRepository extends JpaRepository<CheckResult, Long> {
+    Page<CheckResult> findByMonitorId(Long monitorId, Pageable pageable);
     List<CheckResult> findByMonitorIdOrderByTimestampDesc(Long monitorId);
+    List<CheckResult> findTop10ByMonitorIdOrderByTimestampDesc(Long monitorId);
+    List<CheckResult> findTop20ByMonitorIdOrderByTimestampDesc(Long monitorId);
 }
